734aad7ed523b8aff91d762c6e05825d2eab3b65
Cattatonicat <threeeyedvenelope@gmail.com>
2020-02-07 17:51:05 -0600

    Create SpaceRewind script

---
+++ Building 13/Assets/Scripts/SpaceRewind.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceRewind : MonoBehaviour
+{
+    [SerializeField]
+    private bool leftCollider = false;
+    [SerializeField]
+    private bool rightCollider = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
