8238aa5b234d14860a85400bb50e628d2c774d85
Cattatonicat <threeeyedvenelope@gmail.com>
2020-02-07 18:18:58 -0600

    Add skeleton code in SpaceRewind script

--- Building 13/Assets/Scripts/SpaceRewind.cs
+++ Building 13/Assets/Scripts/SpaceRewind.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class SpaceRewind : MonoBehaviour
 {
+    [Header("Mark the side the collider is on")]
+
+    [Tooltip("Check if the collider is on the left side of the screen")]
     [SerializeField]
     private bool leftCollider = false;
+
+    [Tooltip("Check if the collider is on the right side of the screen")]
     [SerializeField]
     private bool rightCollider = false;
-    // Start is called before the first frame update
-    void Start()
+    
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Check if the GameObject colliding with the space rewind collider is a player
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Player is colliding with space rewind collider on the left side 
+            if (leftCollider)
+            {
+                Debug.Log("Player is collidng with the left space rewind collider.");
+                // Move the player to the right edge of the scene
+                //other.gameObject.transform
+            }
+            // Player is colliding with space rewind collider on the right side 
+            else if (rightCollider)
+            {
+                Debug.Log("Player is colliding with the right space rewind collider.");
+                // Move the player to the left edge of the scene
+            }
+        }
     }
 }
