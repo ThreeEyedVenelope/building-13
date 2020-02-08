07607b4227eccee73376cd24eeeead7836dc199a
Cattatonicat <threeeyedvenelope@gmail.com>
2020-02-08 01:33:03 -0600

    Polish space rewine feature
    * Refactored SpaceRewind
    * Changed the size and location of space rewind colliders

--- Building 13/Assets/Scripts/SpaceRewind.cs
+++ Building 13/Assets/Scripts/SpaceRewind.cs
@@ -4,50 +4,29 @@ using UnityEngine;
 
 public class SpaceRewind : MonoBehaviour
 {
-    [Header("Mark the side the collider is on")]
-
-    [Tooltip("Check if the collider is on the left side of the screen")]
+    [Header("Space Rewind Collider Offset")]
     [SerializeField]
-    private bool leftCollider = false;
-
-    [Tooltip("Check if the collider is on the right side of the screen")]
-    [SerializeField]
-    private bool rightCollider = false;
+    private float offset = 0.2f;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Check if the GameObject colliding with the space rewind collider is a player
         if (other.gameObject.CompareTag("Player"))
         {
-            // Player is colliding with space rewind collider on the left side 
-            if (leftCollider)
+            // Retrieve player's current location
+            Vector2 playerPosition = other.gameObject.transform.position;
+            Debug.Log("Player's current location: " + playerPosition);
+            if (playerPosition.x < 0) // Player is on the left side 
             {
-                Debug.Log("Player is collidng with the left space rewind collider.");
-
-                // Retrieve player's current location
-                Vector2 playerPosition = other.gameObject.transform.position;
-                Debug.Log("Player's current location: " + playerPosition);
-
-                // Move the player to the right edge of the scene 
-                playerPosition.x = 8.5f; // Assign new value to x coordinate
-                other.gameObject.transform.position = playerPosition; // Assign new x coordinate to the player position
-                Debug.Log("Player's new location: " + playerPosition); 
+                playerPosition.x = -playerPosition.x - offset; // Assign new value to x coordinate
             }
-            // Player is colliding with space rewind collider on the right side 
-            else if (rightCollider)
+            else if (playerPosition.x > 0) // Player is on the right side
             {
-                Debug.Log("Player is colliding with the right space rewind collider.");
-
-                // Retrieve player's current location
-                Vector2 playerPosition = other.gameObject.transform.position;
-                Debug.Log("Player's current location: " + playerPosition);
-
-                // Move the player to the left edge of the scene
-                playerPosition.x = -8.5f; // Assign new value to x coordinate 
-                other.gameObject.transform.position = playerPosition; // Assign new x coordinate to the player position
-                Debug.Log("Player's new location: " + playerPosition);
+                playerPosition.x = -playerPosition.x + offset; // Assig new value to x coordinate
             }
-            
+               
+            other.gameObject.transform.position = playerPosition; // Assign new x coordinates to the player position
+            Debug.Log("Player's new location: " + playerPosition);
         }
     }
 }
