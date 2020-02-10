58b5f4f44191505a507eb26bda042df85aa1d0f8
Cattatonicat <threeeyedvenelope@gmail.com>
2020-02-10 13:37:57 -0600

    Add TextTrigger script
    TextTrigger script triggers a world-space UI text to apper when the player enters a trigger zone, and to disappear when the player exits a trigger zone.

--- Building 13/Assets/Scripts/TextTrigger.cs
+++ Building 13/Assets/Scripts/TextTrigger.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class TextTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Drag and drop PopUpText game object.")]
+    [SerializeField]
+    private GameObject popUpText = null;
+
+    void Awake()
     {
-        
+        popUpText.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        // Check if the GameObject colliding with the space rewind collider is a player
+        if (other.gameObject.CompareTag("Player"))
+        {
+            popUpText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            popUpText.SetActive(false);
+        }
     }
 }
