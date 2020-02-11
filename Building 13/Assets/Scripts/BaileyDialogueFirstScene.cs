6006c2f19cd26d73db467a20bea8447b4a9a95cb
Cattatonicat <threeeyedvenelope@gmail.com>
2020-02-11 05:54:38 -0600

    Start BaileyDialogueFirstScene

---
+++ Building 13/Assets/Scripts/BaileyDialogueFirstScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaileyDialogueFirstScene : MonoBehaviour
+{
+    [Header("Drag and drop Bailey's dialogue text UI")]
+    [SerializeField]
+    private Text baileysText =  null;
+
+    [SerializeField]
+    private string baileysDialogue0 = "Hey Fred.";
+
+    private bool kittyMetBailey = false;
+
+    void Awake()
+    {
+        if(!kittyMetBailey)
+        {
+            baileysText.text = baileysDialogue0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
