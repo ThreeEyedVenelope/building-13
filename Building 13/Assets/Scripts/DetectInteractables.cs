07ff265dfdd0b6227481e4a6369bce430c88f4db
Cattatonicat <threeeyedvenelope@gmail.com>
2020-02-15 02:04:13 -0600

    Make DetectInteractables script for Kitty
    TO DO: Make local canvas a child of Kitty to hold Kittys' interaction text.

---
+++ Building 13/Assets/Scripts/DetectInteractables.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectInteractables : MonoBehaviour
+{
+    [SerializeField]
+    private BoxCollider2D playerInteractableArea = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        Debug.Log(other.gameObject.name + " is in Kitty's interactable area.");
+    }
+}
