9245b21137179da44012815675a7f2480c8efb0c
Cattatonicat <threeeyedvenelope@gmail.com>
2020-03-05 16:27:20 -0600

    Add Eyeball's response 1 & 2

---
+++ Building 13/Assets/Scripts/FirstDialogue.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstDialogue : MonoBehaviour
+{
+    public static bool BaileyNeedsToRespond
+    {
+        get { return baileyNeedsToRespond; }
+        set { baileyNeedsToRespond = value; }
+    }
+    public static string BaileyDialogueString
+    {
+        get { return baileyDialogueString; }
+        set { baileyDialogueString = value; }
+    }
+
+    public static string EyeballString
+    {
+        get { return eyeballString;}
+        set { eyeballString = value; }
+    }
+
+    [Header("Universal Dialgoue Game Objects ")]
+    [SerializeField]
+    private GameObject dialogueCanvas = null;
+    [SerializeField]
+    private Image portraitImage = null;
+    [SerializeField]
+    private Text portraitTitle = null;
+
+    [Header("Bailey Dialogue UI")]
+    [SerializeField]
+    private GameObject baileyDialoguePanel = null;
+    [SerializeField]
+    private GameObject baileyPortraitPanel = null;
+    [SerializeField]
+    private Text dialogueText = null;
+
+    [Header("Kitty Dialogue UI")]
+    [SerializeField]
+    private GameObject kittyDialoguePanel = null;
+    [SerializeField]
+    private GameObject kittyPortraitPanel = null;
+
+    [Header("Eyeball Dialogue UI")]
+    [SerializeField]
+    private Sprite eyeballPortraitSprite = null;
+    [SerializeField]
+    private string eyeballName = "Eyeball";
+
+    private static bool baileyNeedsToRespond = false;
+    private static string baileyDialogueString = "";
+
+    private static string eyeballString = "";
+
+
+    private bool dialogueComplete = false;
+    private bool eyeballsTurn = false;
+
+    void Awake()
+    {
+        PlayerController.PlayerCanMove = false;
+        kittyDialoguePanel.SetActive(false);
+        kittyPortraitPanel.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit"))
+        {
+            NextDialogue();
+        }
+    }
+
+    public void NextDialogue()
+    {
+        if (!dialogueComplete)
+        {
+            if (!eyeballsTurn)
+            {
+                if (!baileyNeedsToRespond)
+                {
+                    //Kitty's 1st dialogue choice 
+                    kittyDialoguePanel.SetActive(true);
+                    kittyPortraitPanel.SetActive(true);
+                    baileyDialoguePanel.SetActive(false);
+                    baileyPortraitPanel.SetActive(false);
+                }
+                else if (baileyNeedsToRespond)
+                {
+                    //Bailey is responding to Kitty's 1st dialogue choice. 
+                    //Bailey's response has been set in KittyDialogueChoice.cs
+                    kittyDialoguePanel.SetActive(false);
+                    kittyPortraitPanel.SetActive(false);
+                    baileyDialoguePanel.SetActive(true);
+                    baileyPortraitPanel.SetActive(true);
+                    dialogueText.text = BaileyDialogueString;
+
+                    //Bailey's turn has ended and it is no Eyeball's turn to speak
+                    eyeballsTurn = true;
+                }
+            }
+            else if (eyeballsTurn)
+            {
+                portraitImage.sprite = eyeballPortraitSprite;
+                portraitTitle.text = eyeballName;
+                dialogueText.text = eyeballString;
+                dialogueComplete = true; //The end of the First Dialgoue at the moment!
+            }
+        }
+        else if (dialogueComplete)
+        {
+            dialogueCanvas.SetActive(false);
+            PlayerController.PlayerCanMove = true;
+            Debug.Log("The dialogue has ended.");
+        }
+    }
+
+    public void ResponseSelected()
+    {
+        if(BaileyNeedsToRespond)
+        {
+            baileyDialoguePanel.SetActive(true);
+            baileyPortraitPanel.SetActive(true);
+            kittyDialoguePanel.SetActive(false);
+            kittyPortraitPanel.SetActive(false);
+            dialogueText.text = baileyDialogueString;
+        }
+    }   
+}
--- Building 13/Assets/Scripts/KittyDialogueChoice.cs
+++ Building 13/Assets/Scripts/KittyDialogueChoice.cs
@@ -6,15 +6,17 @@ public class KittyDialogueChoice : MonoBehaviour
 {
     public void BaileyDialogueOneResponseOne()
     {
-        BaileyDialogue.BaileyDialogueString = "I am Lisa. The building manager of Building 13. And you are Fred.";
-        BaileyDialogue.BaileyNeedsToRespond = true;
+        FirstDialogue.BaileyDialogueString = "I am Lisa. The building manager of Building 13. And you are Fred.";
+        FirstDialogue.EyeballString = "Identity theft threat has been detected. Bailey is pretending to be Lisa. Lisa will be here shortly to detain and question Bailey.";
+        FirstDialogue.BaileyNeedsToRespond = true;
         Debug.Log("Kitty chose response number 1");
     }
 
     public void BaileyDialogueOneResponseTwo()
     {
-        BaileyDialogue.BaileyDialogueString = "Ah, you are Fred indeed! It is I the fantastic and wonderous, Lisa!!! I am the building manager of Building 13.";
-        BaileyDialogue.BaileyNeedsToRespond = true;
+        FirstDialogue.BaileyDialogueString = "Ah, you are Fred indeed! It is I the fantastic and wonderous, Lisa!!! I am the building manager of Building 13.";
+        FirstDialogue.EyeballString = "Dear Fred, please step back from the imposter. The imposter’s name is Bailey, and she has triggered an identity theft alert. Lisa will be down shortly to detain and question Bailey.";
+        FirstDialogue.BaileyNeedsToRespond = true;
         Debug.Log("Kitty chose response number 2");
     }
 }
