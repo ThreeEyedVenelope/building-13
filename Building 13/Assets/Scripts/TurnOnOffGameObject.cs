a5fc48914e0f966bee92fb87627299388e009c18
Cattatonicat <threeeyedvenelope@gmail.com>
2020-05-15 10:49:32 -0500

    Create main menu

---
+++ Building 13/Assets/Scripts/TurnOnOffGameObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script turns on/off select game object based on a UNITY UI button. 
+public class TurnOnOffGameObject : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectToOnOff;
+
+    public void TurnOnObject()
+    {
+        objectToOnOff.SetActive(true);
+    }
+
+    public void TurnOffObject()
+    {
+        objectToOnOff.SetActive(false);
+    }
+}
---
+++ Building 13/Assets/Scripts/UIUtility/FadeOut.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour
+{
+    [SerializeField]
+    private float fadeSpeed = 1.0f; // Speed of the fade effect
+
+    private CanvasGroup fadeCanvasGroup;
+
+    private float alpha = 1.0f;
+
+    void Awake()
+    {
+        fadeCanvasGroup = GetComponent<CanvasGroup>();
+        if (fadeCanvasGroup == null)
+            Debug.LogError("[warning] Please add Canvas Group component to the Fade Canvas for the fade effect");
+        if (fadeCanvasGroup != null) fadeCanvasGroup.alpha = alpha;
+    }
+
+    void Update()
+    {
+        if (alpha >= 0.0f)
+        {
+            alpha -= Time.deltaTime * fadeSpeed;
+            fadeCanvasGroup.alpha = alpha;
+            Debug.Log(alpha);
+        }
+    }
+
+
+}
